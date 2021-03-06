1bf19e85ce851eab7fdc9d3785c1448c35356cee
kambar98 <kamilbarul123@gmail.com>
2021-03-06 20:28:34 +0100

    added js code which play audio onkeypress or onclick

--- js/script.js
+++ js/script.js
@@ -25,22 +25,77 @@ window.onload = function ()
 //function which add color to special words of Javascript
 
 function highlight() {
-    var scriptCon = document.getElementById("scriptContainer");
-    var words = div.textContent.split(" ");
-    scriptCon.innerHTML = "";
+    if (window.location.pathname != '/index.html') {
+        var scriptCon = document.getElementById("scriptContainer");
+        var words = div.textContent.split(" ");
+        scriptCon.innerHTML = "";
 
-    for (var i = 0; i < words.length; i++) {
-        if (words[i] === "bad" || words[i] === "good") {
-            words.splice(i, 0, "<span class='jsWord'>");
-            words.splice(i + 2, 0, '</span>');
-            alert(words);
-            if (words[i] === "<span class='jsWord'>" || words[i] === "</span") {
-                i++;
+        for (var i = 0; i < words.length; i++) {
+            if (words[i] === "bad" || words[i] === "good") {
+                words.splice(i, 0, "<span class='jsWord'>");
+                words.splice(i + 2, 0, '</span>');
+                alert(words);
+                if (words[i] === "<span class='jsWord'>" || words[i] === "</span") {
+                    i++;
+                }
             }
         }
+        return scriptCon.innerHTML = words.join(" ")
     }
-
-    return scriptCon.innerHTML = words.join(" ")
+    else
+        return;
 };
 
 // End of function
+
+//drum sound functions
+if (window.location.pathname == '/js1.html') {
+    var clapSound = document.getElementById('clap');
+    var hihatSound = document.getElementById('hihat');
+    var kickSound = document.getElementById('kick');
+    var openhatSound = document.getElementById('openhat');
+    var boomSound = document.getElementById('boom');
+    function pressedA() {
+        clapSound.currentTime = 0;
+        clapSound.play();
+
+    };
+    function pressedS() {
+        hihatSound.currentTime = 0;
+        hihatSound.play();
+    };
+    function pressedD() {
+        kickSound.currentTime = 0;
+        kickSound.play();
+    };
+    function pressedF() {
+        openhatSound.currentTime = 0;
+        openhatSound.play();
+    };
+    function pressedG() {
+        boomSound.currentTime = 0;
+        boomSound.play();
+    };
+    window.addEventListener("keypress", drumFunction);
+    function drumFunction(evt) {
+
+        if (evt.keyCode === 97) {
+            pressedA();
+        }
+        else if (evt.keyCode === 115) {
+            pressedS();
+        }
+        else if (evt.keyCode === 100) {
+            pressedD();
+        }
+        else if (evt.keyCode === 102) {
+            pressedF();
+        }
+        else if (evt.keyCode === 103) {
+            pressedG();
+        }
+
+    };
+};
+
+//End of functiona
